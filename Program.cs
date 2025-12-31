3c14c4e7682fafad83c21227d9f69e68297d68f1
AuDuongTan <auduongtan321@gmail.com>
2025-12-31 13:33:25 +0700

    fix ui for user's booking

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Booking}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
