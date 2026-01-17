7ad658f191ea4bca57f9c420d66d2866ab127047
quysutc2 <6451071063@st.utc2.edu.vn>
2026-01-17 21:45:04 +0700

    .

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=AdminProfile}/{action=Index}/{id?}");
+    pattern: "{controller=AdminFood}/{action=Index}/{id?}");
 
 app.Run();
